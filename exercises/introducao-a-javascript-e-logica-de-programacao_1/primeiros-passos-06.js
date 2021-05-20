c87ab3dc974d9457073ce88eca090f5f83f2be73
Yuri Garrido <y.garrido.yuri@gmail.com>
2021-05-20 13:13:55 -0300

    adiciona exercícios do 7ao 11, faltando terminar o 11.

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/primeiros-passos-06.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/primeiros-passos-06.js
@@ -1,2 +1,22 @@
 //Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
 
+let nomeDaPeca = "bispo";
+
+if (nomeDaPeca.toLocaleLowerCase()=== "bispo"){
+  console.log("Se movimenta em diagonais.");
+} 
+else if(nomeDaPeca.toLocaleLowerCase()=== "cavalo"){
+  console.log("Se movimenta em formato de L.");
+}
+else if(nomeDaPeca.toLocaleLowerCase()=== "peão"){
+  console.log("Se movimenta para a frente.");
+}
+else if(nomeDaPeca.toLocaleLowerCase()=== "rei"){
+  console.log("Se movimenta em qualquer direção, só uma casa.");
+}
+else if(nomeDaPeca.toLocaleLowerCase()=== "rainha"){
+  console.log("Se movimenta para qualquer lado.");
+}
+else if(nomeDaPeca.toLocaleLowerCase()=== "torre"){
+  console.log("Se movimenta em linha reta, sendo vertical ou horizontal.");
+}
