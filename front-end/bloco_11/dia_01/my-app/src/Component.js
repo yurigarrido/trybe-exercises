3840eebb38d681dccced18c9e2fee1f34525031b
Yuri Garrido <y.garrido.yuri@gmail.com>
2021-07-20 19:33:08 -0300

    começa o front-end, e envia o exercicio de fixação da aba -Classes e React

---
+++ front-end/bloco_11/dia_01/my-app/src/Component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Component(){
+  return (
+    <div>
+    <h1>Yuri Miranda Garrido</h1>
+    <p> Olá, estou iniciando minha primeira aplicação com react, em 20/07/2021;</p>
+    </div>
+  )
+}
+
+export default Component;
