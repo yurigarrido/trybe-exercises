634acf6110a9c51d4ff7f65908222269bcf3ab55
yurigarrido <y.garrido.yuri@gmail.com>
2022-01-07 12:44:54 -0300

    adiciona opcao de jogar novamente o jogo do sorteio de numeros

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/exercicio-jogo-adivinhacao/sorteio.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/exercicio-jogo-adivinhacao/sorteio.js
@@ -1,12 +1,24 @@
 const readline = require('readline-sync')
 
-function sorteio () {
-  const numeroSorteado = Math.floor(Math.random() * 10)
-  const numeroEscolhido = readline.questionInt("Escolha um numero de 0 a 10 :   ")
-  let resultado;
-   numeroSorteado === numeroEscolhido ? resultado = "Parabéns, número correto!" : resultado =`Opa, não foi dessa vez. O número era ${numeroSorteado}`;
+function logResultado (numero, resposta) {
+  if (numero !== resposta) {
+    return console.log(`Opa, não foi dessa vez. O número era ${numero}`);
+  }
+  return console.log('Parabéns, número correto!');
 
-   return resultado;
 }
 
-console.log(sorteio());
+function playgame () {
+  const numeroSorteado = Math.floor(Math.random() * 10)
+  const numeroEscolhido = readline.questionInt("Escolha um numero de 0 a 10 :   ")
+  logResultado(numeroSorteado, numeroEscolhido)
+
+  const novamente = readline.question(
+    'Deseja jogar novamente? (Digite s para sim, e qualquer outra coisa para não) '
+  );
+
+  if (novamente === 's') return playgame();
+  console.log('OK, até a próxima!')
+}
+
+playgame();
